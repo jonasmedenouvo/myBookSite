862c7f07ad4cde7920291f4655c535d133d54404
jonasmedenouvo <jonas.medenouvo@lapiscine.pro>
2020-04-01 00:00:07 +0200

    update des liens des livres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,20 +6,20 @@ insert into author (country,last_name,first_name) values (18,"Xin","Feng");
 -- Inserts Book --
 
 insert into book(genre,link_Url,title,id_author) values 
-("11","http://91.165.206.133:44656/share/P9Z-ZRTsRjiQKBMo/Robin%20Hobb%20-%20L'Assassin%20Royal%20-1-%20L'Apprenti%20Assassin.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/BBsDFt-aNGbwfBt1/Robin%20Hobb%20-%20L'Assassin%20Royal%20-2-%20L'Assassin%20du%20Roi.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/J5XDRylJa_mOuOOj/Robin%20Hobb%20-%20L'Assassin%20Royal%20-3-%20La%20Nef%20du%20Cr%C3%A9puscule.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/F-5KI871IZ6qAvXY/Robin%20Hobb%20-%20L'Assassin%20Royal%20-4-%20Le%20Poison%20de%20la%20Vengeance.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/AV9dU1nLDYTy-LGH/Robin%20Hobb%20-%20L'Assassin%20Royal%20-5-%20La%20Voie%20Magique.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/cjzaE8GaIQxB5Dpm/Robin%20Hobb%20-%20L'Assassin%20Royal%20-6-%20La%20Reine%20Solitaire.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/AmDKxaj8f_LblXDu/Robin%20Hobb%20-%20L'Assassin%20Royal%20-7-%20Le%20Proph%C3%A8te%20Blanc.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/JMOSS44il5K3sBKl/Robin%20Hobb%20-%20L'Assassin%20Royal%20-8-%20La%20Secte%20Maudite.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/w46qC9PgnBdy1a93/Robin%20Hobb%20-%20L'Assassin%20Royal%20-9-%20Les%20Secrets%20de%20Castelcerf.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/3jaLtOgGDo-5hlqE/Robin%20Hobb%20-%20L'Assassin%20Royal%20-10-%20Serments%20_%20Deuils.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/0ZHe5o1P1YcVJhkp/Robin%20Hobb%20-%20L'Assassin%20Royal%20-11-%20Le%20Dragon%20des%20Glaces.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/E4JlicCcUl4KWjja/Robin%20Hobb%20-%20L'Assassin%20Royal%20-12-%20L'Homme%20Noir.pdf","L'Apprenti Assassin",
-1),("11","http://91.165.206.133:44656/share/G0BqrtKYmqNX90qQ/Robin%20Hobb%20-%20L'Assassin%20Royal%20-13-%20Adieux%20et%20Retrouvailles.pdf","L'Apprenti Assassin",
+("11","https://drive.google.com/file/d/14utUTgdpYT9QvzYMb5s590YevHgfeYjl/view?usp=sharing","L'Apprenti Assassin",
+1),("11","https://drive.google.com/file/d/1fKa4cz5uQjSNBqEi68I0-Bt7mr-7kLkJ/view?usp=sharing","L'Assassin du Roi",
+1),("11","https://drive.google.com/file/d/12Ry2yAOL2zjigaGnhHI8l95ZrTW8dQY2/view?usp=sharing","La Nef du Crépuscule",
+1),("11","https://drive.google.com/file/d/1f3y_ckAQ_vXH6z14txIe9vDSfCLSPSGu/view?usp=sharing","Le Poison de la Vengeance",
+1),("11","https://drive.google.com/file/d/1wjx3zbscG6yDJqvzWdYeSMMVDvbmtdT3/view?usp=sharing","La Voie Magique",
+1),("11","https://drive.google.com/file/d/1yiQ7TplnnUPOojn4E7IGJtG2-Yi-326N/view?usp=sharing","La Reine Solitaire",
+1),("11","https://drive.google.com/file/d/1xkfiM0lNEPUkHDkDsNVRnuK2_6XdE5g3/view?usp=sharing","Le Prophète Blanc",
+1),("11","https://drive.google.com/file/d/1EuPmoifk1inDwDKvyJpKLWB1uWQive5D/view?usp=sharing","La Secte Maudite",
+1),("11","https://drive.google.com/file/d/1LXuNHnTLJrDcq3ziCNslcJhbRK-TZJf5/view?usp=sharing","Les Secrets de Castelcerf",
+1),("11","https://drive.google.com/file/d/1y2j4jY0kiSq-K54S9XcDbudnkh_ANf1-/view?usp=sharing","Serments Et Deuils",
+1),("11","https://drive.google.com/file/d/17FLgdIYc54WrzQt7yYEIvPyUW6ebXcLF/view?usp=sharing","Le Dragon Des Glaces",
+1),("11","https://drive.google.com/file/d/1vlVQrlgY6XPON2ZerHyBGTnNfROC-UgK/view?usp=sharing","L'Homme Noir",
+1),("11","https://drive.google.com/file/d/1jGAYBDrF6X8bz5f2JRXkNO2POXS2spXV/view?usp=sharing","Adieux Et Retrouvailles",
 1);
 
 insert into book(genre,link_Url,title,id_author) values 
-(18,"https://drive.google.com/open?id=1XHB2robmtdlHyFL8R8Q5ZilLPbaSLMSZ","The Strongest System",2);
+(18,"https://drive.google.com/open?id=1Pje0yiNEGlfWqUZBRik6gmao0FsPTckl","The Strongest System",2);
